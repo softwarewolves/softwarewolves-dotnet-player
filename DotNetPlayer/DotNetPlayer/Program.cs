75fece5ad0c82b406c10633b292f87e0f4d6300d
alexanderhelleboogh <alexander.helleboogh@archiwise.com>
2013-04-15 18:26:26 +0200

    login on server, respond to chatroom invitation

--- DotNetPlayer/DotNetPlayer/Program.cs
+++ DotNetPlayer/DotNetPlayer/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using agsXMPP;
+using agsXMPP.protocol.client;
+using agsXMPP.Xml;
+using agsXMPP.Xml.Dom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using agsXMPP;
-using agsXMPP.Xml;
-using agsXMPP.Xml.Dom;
-using agsXMPP.protocol.client;
+using agsXMPP.protocol.x.muc;
 
 
 namespace DotNetPlayer
@@ -14,43 +15,110 @@ namespace DotNetPlayer
     class DotNetClient
     {
         private XmppClientConnection connection;
+        private string server;
 
         static void Main(string[] args)
         {     
             DotNetClient client = new DotNetClient();
+        //    Console.WriteLine(client.makeChangeRoomPresenceConfirmationMessage("testroom@jabber.org","mynickname","id12345"));
+         
+
             client.Login("jabber.org", "testvillager", "villager");
           //  System.Threading.Thread.Sleep(8000);
             Console.ReadLine();
+        }
+
+        private void outputError(object o, Exception error){
+        
+            Console.WriteLine(error);
         }
 
 
         public void Login(String server, String user, String password)
         {
             connection = new XmppClientConnection(server);
+            connection.OnError += outputError;
+            connection.OnMessage += receiveMessage;
             connection.Open(user, password);
             connection.OnLogin += delegate(object o)
             {
-                sendChatMessage("sww@jabber.org", "I want to play");
+                
+                connection.SendMyPresence();
+                sendChatMessage("sww@"+server, "I want to play");
             };
-            connection.OnMessage += receiveMessage;
+            
+            
         }
 
         public void sendChatMessage(String receiver, String content)
         {
-            connection.Send(new Message(receiver, MessageType.chat, content));
-           
+            Message m = new Message(receiver, MessageType.chat, content);
+            connection.Send(m);
+            Console.WriteLine("Chat message sent: " + m);
         }
 
-        public void joinRoom(Jid chatroom, String content) {
-            Message m = new Message(chatroom, MessageType.groupchat, content);
+        public void sendChatRoomMessage(String room, String content)
+        {
+            Message m = new Message(room + "/testvillager", MessageType.groupchat, content);
+
             connection.Send(m);
-            Console.WriteLine("Yeee room joined: " + chatroom);
+            Console.WriteLine("Chatroom message sent: " + m);
+        }
+
+        public void sendMessage(Element e) {
+            connection.Send(e);
+            Console.WriteLine("Custom message sent: " + e.ToString());
+        }
+
+        private Element makeChangeRoomPresenceConfirmationMessage(String room, String nickname, String id)
+        {
+            Element result = new Element();
+            result.TagName = "presence";
+            result.SetAttribute("id", id);
+            result.SetAttribute("to", room + "/" + nickname);
+
+            Element x = new Element();
+            x.TagName = "x";
+            x.Namespace = "http://jabber.org/protocol/muc";
+            result.AddChild(x);
+
+            Element password = new Element();
+            password.TagName = "password";
+            x.AddChild(password);
+
+            Element history = new Element();
+            history.TagName = "history";
+            history.SetAttribute("maxstanzas", "0");
+            x.AddChild(history);
+
+            return result;
         }
 
         private void receiveMessage(Object sender, Message message)
         {
-            Jid chatroom = message.From;
-            Console.WriteLine("Yeee message received: "+chatroom);
+            Console.WriteLine("Message received: " + message);
+
+            string chatroom=" ";
+            //detect of the message is an invitation to a chatroom
+            if (message.HasTag("invite", true))
+            {
+                chatroom = message.From.ToString();
+                Jid chatroomJid = message.From;
+                
+                Console.WriteLine("Message interpreted as invitation");
+                MucManager manager = new MucManager(connection);
+                manager.JoinRoom(chatroomJid, "mynickname", false);
+                //sendMessage(makeChangeRoomPresenceConfirmationMessage(chatroom,"mynickname","id1"));
+                
+            }
+            else 
+            {
+                Console.WriteLine("Message not interpreted");
+            }
+            
+           
+          
+
         }
 
 
