39e43ea634619b2b93442ea3c4626a9a9e6da57b
alexanderhelleboogh <alexander.helleboogh@archiwise.com>
2013-04-15 23:28:28 +0200

    always voting for JoligeHeidi

---
+++ DotNetPlayer/DotNetPlayer/DecisionStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPlayer
+{
+    class DecisionStrategy
+    {
+        public string vote(string[] candidates)
+        {
+            return candidates.First();
+        }
+    }
+}
---
+++ DotNetPlayer/DotNetPlayer/DotNetClient.cs
@@ -0,0 +1,104 @@
+﻿using agsXMPP;
+using agsXMPP.protocol.client;
+using agsXMPP.Xml;
+using agsXMPP.Xml.Dom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using agsXMPP.protocol.x.muc;
+
+
+namespace DotNetPlayer
+{
+    class DotNetClient
+    {
+        private XmppClientConnection connection;
+        private string server;
+        private string username;
+        private string password;
+        private string nickname;
+        private Jid chatroom;
+
+        static void Main(string[] args)
+        {
+            DotNetClient client = new DotNetClient("awtest1.vm.bytemark.co.uk", "testvillager", "villager", "AngryVillager");
+            client.StartNewGame();
+            Console.ReadLine();
+        }
+
+        private void outputError(object o, Exception error){     
+            Console.WriteLine(error);
+        }
+
+        public DotNetClient(String server, String username, String password, String nickname)
+        {
+            this.server = server;
+            this.username = username;
+            this.password = password;
+            this.nickname = nickname;
+        }
+
+        public void StartNewGame()
+        {
+            connection = new XmppClientConnection(server);
+            connection.OnError += outputError;
+            connection.OnMessage += receiveMessage;
+            connection.Open(username, password);
+            connection.OnLogin += delegate(object o)
+            {        
+              //  connection.SendMyPresence();
+                sendChatMessage("sww@"+server, "I want to play");
+            };
+            
+            
+        }
+
+        private void sendChatMessage(String receiverJID, String content)
+        {
+            Message m = new Message(receiverJID, MessageType.chat, content);
+            connection.Send(m);
+            Console.WriteLine("Chat message sent: " + m);
+        }
+
+        private void sendChatRoomMessage(String content)
+        {
+            Message m = new Message(chatroom, MessageType.groupchat, content);
+            connection.Send(m);
+            Console.WriteLine("Chatroom message sent: " + m);
+        }
+
+
+        private void receiveMessage(Object sender, Message message)
+        {
+            Console.WriteLine("Message received: " + message);
+
+            //detect of the message is an invitation to a chatroom
+            if (message.HasTag("invite", true))
+            {
+                Console.WriteLine("Message interpreted as invitation");
+                chatroom = message.From;
+                MucManager manager = new MucManager(connection);
+                manager.JoinRoom(chatroom, nickname, false);
+            }
+            else if(message.Body!=null && message.Body.ToString().StartsWith("Please vote who should be hanged"))
+            {
+                sendChatRoomMessage("I vote for JoligeHeidi");
+                Console.WriteLine("Voted for JoligeHeidi");
+
+            } else
+            {
+                Console.WriteLine("Message not interpreted");
+            }
+            
+           
+          
+
+        }
+
+
+
+    }
+}
+
